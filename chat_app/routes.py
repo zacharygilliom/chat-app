4faa29a9605345b4f0fba3a0130f9c48a7e45375
zacharygilliom <zacharygilliom@gmail.com>
2020-04-20 19:32:59 -0400

    added login validation.

--- chat_app/routes.py
+++ chat_app/routes.py
@@ -1,5 +1,5 @@
 from chat_app.models import User, Posts
-from flask import request, render_template, redirect
+from flask import request, render_template, redirect, flash, url_for
 from chat_app.forms import RegistrationForm, LoginForm
 from chat_app import app
 from chat_app import db
@@ -18,16 +18,7 @@ def login():
 
 @app.route('/user_home', methods=['GET', 'POST'])
 def user_page():
-    if request.method == 'POST':
-        db.create_all()
-        username = request.form['validationCustomUsername']
-        password = request.form['exampleInputPassword1']
-        firstname = request.form['userFirstName']
-        lastname = request.form['userLastName']
-        new_user = User(username=username, password=password, firstname=firstname, lastname=lastname)
-        db.session.add(new_user)
-        db.session.commit()
-    return render_template('user_home.html', username=username, password=password)
+    return render_template('user_home.html') 
 
 
 @app.route('/user_info', methods=['GET'])
@@ -39,8 +30,11 @@ def user_info_page():
 def posts():    
     return render_template('posts.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account create for {form.username.data}!', 'success')
+        return redirect(url_for('user_page'))
     return render_template('register.html', form=form)
 
